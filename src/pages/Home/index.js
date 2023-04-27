4a2edc321cdb40950eca080b652f3e7ceb595adb
David Mello <davidbezerra10@hotmail.com>
2023-04-27 12:46:22 -0300

    Estruturação da página inicial e outras.
    Estruturei a página inicial com as capas e nomes dos filmes, e criei a página de erro.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,7 @@ import './home.css'
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -22,11 +23,20 @@ function Home(){
 
             //console.log(response.data.results.slice(0, 10))
             setFilmes(response.data.results)
+            setLoading(false)
         }
 
         loadFilmes();
 
     })
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
 
     return(
         <div className="container">
