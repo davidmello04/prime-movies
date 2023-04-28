9ebc0a19ec7ea837253ca7a9a9f17b0dcb181b82
David Mello <davidbezerra10@hotmail.com>
2023-04-28 01:37:07 -0300

    Responsividade e Criei a página de detalhes dos filmes.

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,74 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import './filme-info.css'
+import api from '../../services/api'
 
 function Filme(){
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const [filme, setFilme] = useState({})
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: "718a55e25ad423913e1cb71004945684",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false)
+            })
+            .catch(()=> {
+                console.log("Filme não encontrado!")
+                navigate('/', { replace: true })
+                return;
+            })
+        }
+
+        loadFilme()
+
+        return() => {
+            console.log("Componente foi desmontado!")
+        }
+    }, [navigate, id])
+
+
+    function salvarFilme(){
+        alert("Teste")
+    }
+
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando detalhes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {Number(filme.vote_average).toFixed(1)} / 10</strong>
+
+            <div className="area-buttons">
+                <button onClick={salvarFilme}>Salvar</button>
+                <button>
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
