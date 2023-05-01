4b0742ccdd1c53bd7afec30bc22a62f1bb040d1c
David Mello <davidbezerra10@hotmail.com>
2023-04-30 22:25:54 -0300

    Ajuste _blank

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer(){
                 <p id="footer">
                     Feito por David Melo | Utilizando a API themoviedb.
                 </p>
-                <small>Os dados foram obtidos da API <a href="https://www.themoviedb.org" target="_blank">themoviedb</a>, uma fonte pública de
+                <small>Os dados foram obtidos da API <a href="https://www.themoviedb.org" target="blank">themoviedb</a>, uma fonte pública de
                     informações sobre Filmes.</small>
             </footer>
         </div>
